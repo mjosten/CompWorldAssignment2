b3b7d11afa2e406640f099f5719fad5b65762afc
mjosten <38707699+mjosten@users.noreply.github.com>
2019-02-20 21:20:09 -0800

    game of life with the gosper gun

---
+++ gameengine.js
@@ -0,0 +1,178 @@
+// This game shell was happily copied from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
+
+window.requestAnimFrame = (function () {
+    // return window.requestAnimationFrame ||
+    //         window.webkitRequestAnimationFrame ||
+    //         window.mozRequestAnimationFrame ||
+    //         window.oRequestAnimationFrame ||
+    //         window.msRequestAnimationFrame ||
+            return function (/* function */ callback, /* DOMElement */ element) {
+                window.setTimeout(callback, 1000 / 30);
+            };
+})();
+
+class Timer {
+    constructor() {
+        this.gameTime = 0;
+        this.maxStep = 0.05;
+        this.wallLastTimestamp = 0;
+    }
+    tick() {
+        var wallCurrent = Date.now();
+        var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+        this.wallLastTimestamp = wallCurrent;
+
+        var gameDelta = Math.min(wallDelta, this.maxStep);
+        this.gameTime += gameDelta;
+        return gameDelta;
+    }
+}
+
+class GameEngine {
+    constructor() {
+        this.entities = [];
+        this.showOutlines = false;
+        this.board = null;
+        this.ctx = null;
+        //this.click = {x:0, y:0};
+        this.click = null;
+        this.surfaceWidth = null;
+        this.surfaceHeight = null;
+    }
+    init(ctx) {
+        this.ctx = ctx;
+        this.surfaceWidth = this.ctx.canvas.width;
+        this.surfaceHeight = this.ctx.canvas.height;
+        this.startInput();
+        this.timer = new Timer();
+        console.log('game initialized');
+    }
+    start() {
+        console.log("starting game");
+        var that = this;
+        (function gameLoop() {
+            that.loop();
+            requestAnimFrame(gameLoop, that.ctx.canvas);
+        })();
+    }
+    startInput() {
+        console.log('Starting input');
+        var that = this;
+
+        var getXandY = function (e) {
+            var x = e.clientX - that.ctx.canvas.getBoundingClientRect().left;
+            var y = e.clientY - that.ctx.canvas.getBoundingClientRect().top;
+
+            return { x: x, y: y };
+        }
+
+        // this.ctx.canvas.addEventListener("keydown", function (e) {
+        //     //console.log(e);
+        //     console.log("Key Down Event - Char " + e.code + " Code " + e.keyCode);
+        //     e.preventDefualt();
+        // }, false);
+        document.addEventListener('keydown', function(e) {
+            //console.log("Key Down Event - Char " + e.code + " Code " + e.keyCode);
+            if (e.code == "Space" && !that.space) {
+                that.space = true;
+            }
+            else if (e.code == "Space" && that.space) {
+                that.space = false;
+            }
+            e.preventDefault();
+        });
+
+        // this.ctx.canvas.addEventListener("keyup", function (e) {
+        //     console.log("Key Up Event - Char " + e.code + " Code " + e.keyCode);
+        //     //that.keys[e.code] = false;
+        //     e.preventDefault();
+        // }, false);
+
+        this.ctx.canvas.addEventListener("click", function (e) {
+            //console.log(getXandY(e));
+            that.click = getXandY(e);
+        }, false);
+
+        // this.ctx.canvas.addEventListener("keydown", function (e) {
+        //     console.log("Key Down Event - Char " + e.code + " Code " + e.keyCode);
+        //     //that.keys[e.code] = true;
+        //     if (e.code == "Space" && !that.space) {
+        //         that.space = true;
+        //     } 
+        //     else if (e.code == "Space" && that.space) {
+        //         that.space = false;
+        //     }
+        // }, false);
+
+        
+
+        
+    }
+    addEntity(entity) {
+        console.log('added entity');
+        this.entities.push(entity);
+    }
+    draw() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.save();
+        for (var i = 0; i < this.entities.length; i++) {
+            this.entities[i].draw(this.ctx);
+        }
+        this.ctx.restore();
+    }
+    update() {
+        var entitiesCount = this.entities.length;
+
+        for (var i = 0; i < entitiesCount; i++) {
+            var entity = this.entities[i];
+
+            if (!entity.removeFromWorld) {
+                entity.update();
+            }
+        }
+
+        for (var i = entitiesCount - 1; i >= 0; --i) {
+            if (this.entities[i].removeFromWorld) {
+                this.entities.splice(i, 1);
+            }
+        }
+    }
+    loop() {
+        this.clockTick = this.timer.tick();
+        this.update();
+        this.draw();
+        this.click = null;
+        
+    }
+    
+}
+class Entity {
+    constructor(game, x, y) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.removeFromWorld = false;
+    }
+    update() {
+
+    }
+    draw() {
+        
+    }
+    rotateAndCache(image, angle) {
+        var offscreenCanvas = document.createElement('canvas');
+        var size = Math.max(image.width, image.height);
+        offscreenCanvas.width = size;
+        offscreenCanvas.height = size;
+        var offscreenCtx = offscreenCanvas.getContext('2d');
+        offscreenCtx.save();
+        offscreenCtx.translate(size / 2, size / 2);
+        offscreenCtx.rotate(angle);
+        offscreenCtx.translate(0, 0);
+        offscreenCtx.drawImage(image, -(image.width / 2), -(image.height / 2));
+        offscreenCtx.restore();
+        //offscreenCtx.strokeStyle = "red";
+        //offscreenCtx.strokeRect(0,0,size,size);
+        return offscreenCanvas;
+    }
+}
---
+++ main.js
@@ -0,0 +1,313 @@
+/*
+* Michael Josten
+* Computational World 
+* Assignment 2 - Emergence/Interaction
+* Game of Life
+*/
+
+const DIMENSION = 40;
+
+const STATE1= [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+[0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];               
+
+function randomInt(n) {
+    return Math.floor(Math.random() * n);
+}
+
+function rgb(r, g, b) {
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+//Cells that are just born should not be checked as a neighbor.
+//create next generation state of the board then checks the current state of each cell
+//then transfers the state over, rather than sequentially updating every cell
+//each cell should be simultaneously updated to next state rather than sequentially.
+class Cell {
+    constructor(game, board, row, col, alive) {
+        this.game = game;
+        this.row = row;
+        this.col = col;
+        this.removeFromWorld = false;
+        this.alive = alive;
+        this.board = board;
+        //default color to white
+        this.color = rgb(255, 255, 255);
+        //if alive then set to black.
+        if (this.alive) 
+            this.color = rgb(0, 0, 0);
+
+        
+    }
+    update(nextBoard) {
+        //console.log(`row=${this.row}, col=${this.col}, alive=${this.alive}`);
+        var aliveCount = this.aliveNeighbors();
+
+        var nextState = new Cell(this.game, this.board, this.row, this.col, this.alive);
+        
+        //any cell with fewer than 2 neighbors dies
+        //any cell with more than 3 neighbors dies 
+        if (this.alive && (aliveCount < 2 || aliveCount > 3)) {
+            nextState.alive = 0;
+        }
+        //any cell with exactly 3 neighbors becomes alive
+        else if ((!(this.alive)) && (aliveCount === 3)) {
+            nextState.alive = 1;
+        } 
+        //any cell with 2-3 neighbors stays alive
+        else if (aliveCount >= 2 && aliveCount <= 3) {
+        }
+        //set color
+        if (!nextState.alive)
+            nextState.color = rgb(255, 255, 255);
+        else 
+            nextState.color = rgb(0, 0, 0);
+        
+        nextBoard[nextState.row][nextState.col] = nextState;
+        //nextBoard[nextState.row][nextState.col].push(nextState);
+        
+    }
+    draw() {
+    }
+    
+    aliveNeighbors() {
+        var count = 0;
+        var board = this.board.board;
+        var minI = -1;
+        var minJ = -1;
+        var maxI = 1;
+        var maxJ = 1;
+        //count neighbor alive status
+
+        // Top Left Cell
+        if (this.col - 1 < 0 && this.row - 1 < 0) {
+            minJ = 0;
+            minI = 0;
+        }
+        // Top right Cell
+        else if (this.col + 1 >= this.board.dimension && this.row -1 < 0) {
+            maxJ = 0;
+            minI = 0;
+        }
+        // Bottom Left Cell
+        else if (this.col -1 < 0 && this.row + 1 >= this.board.dimension) {
+            minJ = 0;
+            maxI = 0;
+        }
+        // Bottom Right Cell
+        else if (this.col + 1 >= this.board.dimension && this.row + 1 >= this.board.dimension) {
+            maxJ = 0;
+            maxI = 0;
+        }
+
+        // Cell in left column
+        else if (this.col - 1 < 0) {
+            minJ = 0;
+        }
+        // Cell in right column
+        else if (this.col + 1 >= this.board.dimension) {
+            maxJ = 0;
+        }
+        //cell in top row
+        else if (this.row - 1 < 0) {
+            minI = 0;
+
+        }
+        // Cell in the bottom row
+        else if (this.row + 1 >= this.board.dimension) {
+            maxI = 0;
+        }
+        
+        //count the neighbors
+        for (var i = minI; i <= maxI; i++) {
+            for (var j = minJ; j <= maxJ; j++) {
+                //console.log(`i=${i}, j=${j}, maxI=${maxI}, maxJ=${maxJ}`);
+                count += board[this.row + i][this.col + j].alive;
+                //console.log(`count = ${count}; board[${this.row + i}][${this.col + j}] = 
+                //${board[this.row + i][this.col + j].alive}`);
+            }
+        }
+        
+        //subtract itself 
+        count -= board[this.row][this.col].alive;
+        return count;
+    }
+    
+}
+
+class GameBoard extends Entity {
+    constructor(game) {
+        super(game, 0, 0);
+        console.log("make gameboard");
+        this.dimension = DIMENSION;
+        //cells are either alive(1) or dead (0)
+
+        //create board
+        this.board = [];
+        this.init();
+        //this.initCells();
+    
+    }
+    init() {
+        for (var row = 0; row < this.dimension; row++) {
+            this.board.push([]);
+            for (var col = 0; col < this.dimension; col++) {
+                this.board[row].push(new Cell(this.game, this, row, col, STATE1[col][row]));
+            }
+        }
+        
+    }
+    initCells() {
+        // for (var row = 0; row < STATE1.length; row++) {
+        //     for (var col = 0; col < STATE1[row].length; col++) {
+        //         this.setCell(row, col, STATE1[row][col]);
+        //         //this.board[row][col].dead = STATE1[row][col];
+        //     }
+        // }
+        //this.createGlider(5, 5);
+
+
+    }
+    setCell(row, col, alive) {
+        this.board[row][col].alive = alive;
+    }
+
+    update() {
+        super.update();
+        var nextBoard = [];
+
+
+    
+        if (this.game.click || this.game.space) {
+            for (var row = 0; row < this.board.length; row++) {
+                nextBoard.push([]);
+                for (var col = 0; col < this.board[row].length; col++) {
+                    this.board[row][col].update(nextBoard);
+                }
+            }
+            this.board = nextBoard;
+            
+        }
+        
+    }
+
+    createGlider(row, col) {
+        this.setCell(row, col, 0);
+        this.setCell(row+1, col+1, 0);
+        this.setCell(row+1, col+2, 0);
+        this.setCell(row, col+2, 0);
+        this.setCell(row-1, col+2, 0);
+    }
+
+    draw(ctx) {
+        super.draw();
+        //size of cell is surfaceWidth / dimension by surfaceHeight / dimension
+        //draw the board as a grid
+        var cellWidth = this.game.surfaceWidth / this.dimension;
+        var cellHeight = this.game.surfaceHeight / this.dimension;
+        var lineWidth = 1
+        
+        ctx.lineWidth = lineWidth;
+        //draw vertical grid lines
+        for (var i = 0; i <= this.dimension + 1; i++) {
+            //ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(i * cellWidth, 0);
+            ctx.lineTo(i * cellWidth, cellHeight * this.dimension);
+            ctx.stroke();
+            ctx.closePath();
+            //ctx.restore();
+        }
+        //draw horizontal grid lines
+        for (var i = 0; i <= this.dimension + 1; i++) {
+            ctx.beginPath();
+            ctx.moveTo(0, i * cellHeight);
+            ctx.lineTo(cellWidth * this.dimension, i * cellHeight);
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+        //paint the cells
+        for (var row = 0; row < this.board.length; row++) {
+            for (var col = 0; col < this.board[row].length; col++) {
+                ctx.beginPath();
+                ctx.fillStyle = this.board[col][row].color;
+                ctx.fillRect(col * cellWidth + lineWidth, 
+                    row * cellHeight + lineWidth, 
+                    cellWidth - lineWidth * 2, 
+                    cellHeight - lineWidth * 2);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+
+    }
+}
+
+
+function main() {
+    console.log("in main");
+    // var ASSET_MANAGER = new AssetManager();
+
+    // ASSET_MANAGER.downloadAll(function () {
+    //     var canvas = document.getElementById('GameOfLife');
+    //     var ctx = canvas.getContext('2d');
+
+
+    //     var gameEngine = new GameEngine();
+    //     var gameBoard = new GameBoard(gameEngine);
+    //     gameEngine.addEntity(gameBoard);
+    //     gameEngine.board = gameBoard;
+    //     gameEngine.init(ctx);
+    //     gameEngine.start();
+    // });
+    var canvas = document.getElementById('GameOfLife');
+    var ctx = canvas.getContext('2d');
+    var gameEngine = new GameEngine();
+    var gameBoard = new GameBoard(gameEngine);
+    //gameBoard.init();
+    gameEngine.addEntity(gameBoard);
+    gameEngine.board = gameBoard;
+    gameEngine.init(ctx);
+    gameEngine.start();
+}
+
+main();
